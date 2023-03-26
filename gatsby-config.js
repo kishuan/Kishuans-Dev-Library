4a5e3a8dce6173227d8793067948bb9e19f8ac61
Kishuan Matteo <kish@Kishuans-MacBook-Pro.local>
2023-03-26 13:53:30 -0600

    Installed MUI

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,17 @@
  * @type import('gatsby').GatsbyConfig
  */
 
-require("dotenv").config()
+require("dotenv").config();
+
+const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
+const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
+
+if (!CONTENTFUL_ACCESS_TOKEN || !CONTENTFUL_SPACE_ID) {
+  throw new Error(
+    "Missing environment variables. Please set CONTENTFUL_ACCESS_TOKEN and CONTENTFUL_SPACE_ID in your .env file"
+  );
+}
+
 
 module.exports = {
   siteMetadata: {
@@ -45,8 +55,8 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: CONTENTFUL_ACCESS_TOKEN,
+        spaceId: CONTENTFUL_SPACE_ID,
       },
     },
   ],
