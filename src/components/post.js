472fdabb67559bf7c209e6bed1ac90f42648fdb8
Kishuan Matteo <kish@Kishuans-MacBook-Pro.local>
2023-04-02 21:15:47 -0600

    Update Post.js

--- src/components/post.js
+++ src/components/post.js
@@ -3,7 +3,6 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Box from "@mui/material/Box"
 import Chip from "@mui/material/Chip"
 import Stack from "@mui/material/Stack"
-import Paper from "@mui/material/Paper"
 
 const Post = ({ title, description, updatedAt, tag }) => {
   return (
@@ -28,30 +27,29 @@ const Post = ({ title, description, updatedAt, tag }) => {
       <hr></hr>
       <div>{documentToReactComponents(JSON.parse(description.raw))}</div>
       <hr></hr>
-      <Paper elevation={0} sx={{ backgroundColor: `#474E68`, padding: "0.5em"}}>
-        <Stack direction="row" spacing={1}>
-          <Chip
-            sx={{
-              height: "auto",
-              "& .MuiChip-label": {
-                display: "block",
-                whiteSpace: "normal",
-              },
-              backgroundColor: `#474E68`,
-              color: `#ECF2FF`,
-            }}
-            label={`Last published: ${updatedAt}`}
-            size="small"
-            variant="outlined"
-          />
-          <Chip
-            sx={{ color: `#ECF2FF`, backgroundColor: `#474E68` }}
-            label={tag}
-            size="small"
-            variant="outlined"
-          />
-        </Stack>
-      </Paper>
+
+      <Stack direction="row" spacing={1}>
+        <Chip
+          sx={{
+            height: "auto",
+            "& .MuiChip-label": {
+              display: "block",
+              whiteSpace: "normal",
+            },
+            backgroundColor: `#474E68`,
+            color: `#ECF2FF`,
+          }}
+          label={`Last published: ${updatedAt}`}
+          size="small"
+          variant="outlined"
+        />
+        <Chip
+          sx={{ color: `#ECF2FF`, backgroundColor: `#474E68` }}
+          label={tag}
+          size="small"
+          variant="outlined"
+        />
+      </Stack>
     </Box>
   )
 }
